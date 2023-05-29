9bd5ce209adbdc6b3e97610332ac801dcd7d0c57
Matt Grimes <matt@mtgrimes.com>
2023-05-29 16:04:40 -0600

    Part 1 working

--- src/day3.rs
+++ src/day3.rs
@@ -1,18 +1,48 @@
 use crate::utils::get_lines;
+use std::cmp::{min, max};
 
 pub fn day3(file_path: String) {
     let lines = get_lines(file_path);
+
     let path0 = parse_path(&lines[0]);
     let path1 = parse_path(&lines[1]);
+
     let mut intersections: Vec<(i32, i32)> = Vec::new();
-    for coord0 in &path0 {
-        for coord1 in &path1 {
-            if coord0 == coord1 {
-                intersections.push(*coord0);
-            }
-        }
-    }
-    println!("{:?}", intersections);
+	for i in 0..path0.len()-1 {
+		let node00 = path0[i];
+		let node01 = path0[i+1];
+		for j in 0..path1.len()-1 {
+			let node10 = path1[j];
+			let node11 = path1[j+1];
+			if node00.0 == node01.0 { // first edge is vertical
+				if node10.1 == node11.1 { // second edge is horizontal
+					if check_intersection(&[node10, node11], &[node00, node01]){
+						intersections.push((node00.0,node10.1));
+					}
+				}
+			}
+			else { // first edge is horizontal
+				if node10.0 == node11.0 { // second edge is vertical
+					if check_intersection(&[node00, node01], &[node10, node11])
+					{
+						intersections.push((node10.0,node00.1));
+					}
+				}
+			}
+		}
+	}
+	println!("{:?}", intersections.iter().map(|x| x.0.abs() + x.1.abs()).filter(|x| x>&(0 as i32)).min().unwrap());
+}
+
+fn check_intersection(horizontal: &[(i32,i32); 2], vertical: &[(i32,i32); 2]) -> bool {
+	let left = min(horizontal[0].0, horizontal[1].0);
+	let right = max(horizontal[0].0, horizontal[1].0);
+	let bottom = min(vertical[0].1, vertical[1].1);
+	let top = max(vertical[0].1, vertical[1].1);
+	if vertical[0].0 <= right && vertical[0].0 >= left && horizontal[0].1 <= top && horizontal[0].1 >= bottom {
+		return true
+	}
+	false
 }
 
 fn parse_path(path: &str) -> Vec<(i32, i32)> {
@@ -23,7 +53,6 @@ fn parse_path(path: &str) -> Vec<(i32, i32)> {
         let current_coord = coords.last().unwrap();
         coords.push(get_new_coord(current_coord, step));
     }
-    println!("{:?}", coords);
     coords
 }
 
